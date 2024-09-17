e51623e87d0d26cf8148f4f76540611d62446c18
AmalieKA <a.ka@live.dk>
2024-09-17 11:53:45 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <h1>Pepita's energy:</h1>
+  <img
+    src="https://en.wikipedia.org/wiki/Swallow#/media/File:Pied-winged_swallow_(Hirundo_leucosoma).jpg"
+  />
+</template>
+
+<style></style>
